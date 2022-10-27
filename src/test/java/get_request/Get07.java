a8644f6c97b183129e36628f42bb6515e80c1a1c
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-27 22:55:42 +0300

    27Eki22-22:55

---
+++ src/test/java/get_request/Get07.java
@@ -0,0 +1,21 @@
+package get_request;
+
+public class Get07 {
+
+     /*
+      Given
+       https://jsonplaceholder.typicode.com/todos
+    When
+        I send GET Request to the URL == > URL'e Get Request gonderin
+    Then
+        1)Status code is 200 == > Status kodu 200 olmali
+        2)Print all ids greater than 190 on the console ==> id si 190 dan buyuk olanlari konsola yazdirin
+          Assert that there are 10 ids greater than 190 == > 10 tane id nin 190 dan buyuk oldugunu dogrulayin
+        3)Print all userIds whose ids are less than 5 on the console ==> id si 5 den kucuk olan tum userid lerini konsolunu yazdirin
+          Assert that the number of userIds whose ids are less than 5 is 4 ==> id si 5 den kucuk olan 4 tane userId oldugunu dogrulayin
+        4)Print all titles whose ids are less than 5 ==> ıd si 5 den kucuk olan tum basliklari yazdirin
+          Assert that "delectus aut autem" is one of the titles whose id is less than 5 ==> id si 5 den kucuk olan datalarin birinin
+          basliginin "delectus aut autem" icerdigini dogrulayin
+   */
+
+}
