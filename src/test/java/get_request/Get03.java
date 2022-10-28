e3ca52b077051580f0378f97ca33399663831b46
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-28 15:35:55 +0300

    28Eki22-15:35

--- src/test/java/get_request/Get03.java
+++ src/test/java/get_request/Get03.java
@@ -18,14 +18,14 @@ public class Get03 extends JsonplaceholderBaseUrl {
             User send GET Request to the URL
         Then
             HTTP Status Code should be 200
-      And
-          Response format should be "application/json"
-      And
-          "title" is "et itaque necessitatibus maxime molestiae qui quas velit",
-      And
-          "completed" is false
-      And
-          "userId" is 2
+        And
+            Response format should be "application/json"
+        And
+            "title" is "et itaque necessitatibus maxime molestiae qui quas velit",
+        And
+            "completed" is false
+        And
+            "userId" is 2
      */
 
     @Test
@@ -39,28 +39,29 @@ public class Get03 extends JsonplaceholderBaseUrl {
 
         // Send the request and Get Response
 
-        Response response=given().spec(spec).when().get("/{first}/{second}");
+        Response response = given().spec(spec).when().get("/{first}/{second}");
         //response.prettyPrint();
 
         // Do Assertion
 
         // 1. Yol ( Hard Assert)
-        response.then().
-                assertThat().
-                statusCode(200).
-                contentType("application/json").
-                body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit")).
-                body("completed",equalTo(false)).
-                body("userId",equalTo(2));
+        response.then()
+                 .assertThat()
+                 .statusCode(200)
+                 .contentType("application/json")
+                 .body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"))
+                 .body("completed",equalTo(false))
+                 .body("userId",equalTo(2));
 
         // 2. Yol Sadece body içerisinde geçerli bir (Soft Assert)
-
-        response.then().
-                assertThat().
-                statusCode(200).
-                contentType(ContentType.JSON).
-                body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"),
-                        "completed",equalTo(false),"userId",equalTo(2));
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"),
+                      "completed",equalTo(false),
+                      "userId",equalTo(2));
 
 
     }
