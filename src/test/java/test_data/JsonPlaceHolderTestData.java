65351a1fce0e961079089930c02ecfcae0a68c66
selcukAltinisik <selcukaltin3544@gmail.com>
2022-11-05 13:26:13 +0300

    05Kas22-13:26

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -6,12 +6,23 @@ import java.util.Map;
 public class JsonPlaceHolderTestData {
 
     public Map<String,Object> expectedDataMethod(Integer userId, String title, Boolean completed){
+
         Map<String,Object> expectedDataMap = new HashMap<>();
-        expectedDataMap.put("userId",1);
-        expectedDataMap.put("title", "quis ut nam facilis et officia qui");
-        expectedDataMap.put("completed", false);
+        if (userId!=null){
+            expectedDataMap.put("userId",1);
+
+        }
+        if (title!=null){
+            expectedDataMap.put("title", title);
+
+        }
+        if (completed!=null){
+            expectedDataMap.put("completed", false);
+        }
 
         return expectedDataMap;
     }
+
+
 
 }
